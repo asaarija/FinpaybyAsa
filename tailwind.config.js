5b84addb7feb18348e612bd935f25590705a770e
asaarija <imanasa09@gmail.com>
2025-07-15 21:23:52 +0700

    Add initial project files (HTML, CSS, assets)

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./src/**/*.{html,js}"], // Sesuaikan dengan struktur folder Anda
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
